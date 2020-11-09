cb311b919772f20763a29282432f5a6ef9d9ad77
John A Stevenson <jostev@bgs.ac.uk>
2020-11-09 14:00:36 +0000

    Separate import for plots (still works)

--- _posts/demo.py
+++ _posts/demo.py
@@ -6,6 +6,8 @@ from pathlib import Path
 from matplotlib import pyplot as plt
 import numpy as np
 
+from my_plots import my_imshow
+
 logger = logging.getLogger("demo")
 
 
@@ -31,7 +33,7 @@ def plot_single_figure(data_slice, title, output_dir):
     filename = output_dir / f"{title}.png"
     logger.info("Plotting %s with PID %s", filename, os.getpid())
 
-    plt.imshow(data_slice)
+    my_imshow(data_slice)
     plt.title(title)
     plt.savefig(filename)
     plt.close()
---
+++ _posts/my_plots.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+
+
+def my_imshow(data):
+    return plt.imshow(data)
