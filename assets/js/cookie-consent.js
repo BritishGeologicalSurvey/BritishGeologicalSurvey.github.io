139186c19cb1ae90701b2fc92b40bac26a5ff50a
Steve Richardson <stever@bgs.ac.uk>
2021-11-04 12:20:31 +0000

    feat: added ga and opt in or out popup:

---
+++ assets/js/cookie-consent.js
@@ -0,0 +1,44 @@
+window.cookieconsent.initialise({
+  palette: {
+    popup: {
+      background: '#e3e9f2',
+      text: '#002e40',
+    },
+    button: {
+      background: '#002e40',
+      text: '#ffffff',
+    },
+  },
+  onInitialise: function (status) {
+    var type = this.options.type;
+    var didConsent = this.hasConsented();
+    if (type == 'opt-in' && didConsent) {
+      // enable cookies
+      loadGAonConsent();
+    }
+    if (type == 'opt-out' && !didConsent) {
+      // disable cookies
+    }
+  },
+  onStatusChange: function (status, chosenBefore) {
+    var type = this.options.type;
+    var didConsent = this.hasConsented();
+    if (type == 'opt-in' && didConsent) {
+      // enable cookies
+      loadGAonConsent();
+    }
+    if (type == 'opt-out' && !didConsent) {
+      // disable cookies
+    }
+  },
+  onRevokeChoice: function () {
+    var type = this.options.type;
+    if (type == 'opt-in') {
+      // disable cookies
+    }
+    if (type == 'opt-out') {
+      // enable cookies
+      loadGAonConsent();
+    }
+  },
+});
